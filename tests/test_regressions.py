fdecc400b9acaaf22c74e5833f5dfbd1feed4295
Martin-Daniel Lacasse <mdlacasse@users.noreply.github.com>
2024-12-09 00:30:41 -0500

    made tests year independent

--- tests/test_regressions.py
+++ tests/test_regressions.py
@@ -1,5 +1,7 @@
 
 import pytest
+import sys
+from datetime import date
 
 import owlplanner as owl
 
@@ -32,16 +34,19 @@ def test_constructor2():
     assert p._name == name
 
 def test_withdrawal1():
+    thisyear = date.today().year
     inames = ['Joe']
     yobs = [1964]
-    expectancy = [62]
+    # This makes three years to fund.
+    expectancy = [thisyear - 1964 + 2]
     name = 'withdrawal_1'
     p = owl.Plan(inames, yobs, expectancy, name, startDate='1/1')
     p.setSpendingProfile('flat')
-    p.setAccountBalances(taxable=[0], taxDeferred=[37.5], taxFree=[0])
+    threshold = 37.5
+    p.setAccountBalances(taxable=[0], taxDeferred=[threshold], taxFree=[0])
     p.setAllocationRatios('individual', generic=[[[0, 0, 0, 100], [0, 0, 0, 100]]])
     p.setRates('fixed', values=[0, 0, 0, 0])
     options = {'maxRothConversion': 0, 'solver': solver}
     p.solve('maxSpending', options=options)
-    assert p.basis == pytest.approx(1000*37.5/3, 0.01)
+    assert p.basis == pytest.approx(1000*threshold/3, 0.01)
 
