1e842e60d2c96f7996340da72b362bd5f6c1f97b
Martin-Daniel Lacasse <mdlacasse@users.noreply.github.com>
2024-12-27 20:20:26 -0500

    provide .ini example

---
+++ ui/About_Owl.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+import sskeys as k
+
+ret = k.titleBar('MC')
+st.write("## About Owl")
+
+st.write('#### 🦉 This version of Owl was release in December 2024.')
+
+st.divider()
+st.write('Copyright &copy; 2024 Martin-D. Lacasse')
--- ui/main.py
+++ ui/main.py
@@ -20,7 +20,9 @@ pages = {
           'Multiple scenarios': [st.Page('Historical_Range.py', icon=':material/earthquake:'),
                                  st.Page('Monte_Carlo.py', icon=':material/stacked_bar_chart:')],
           'Resources': [st.Page('Logs.py', icon=':material/error:'),
-                        st.Page('Documentation.py', icon=':material/question_mark:')],
+                        st.Page('Documentation.py', icon=':material/question_mark:'),
+                        st.Page('About_Owl.py', icon=':material/nature:')],
+                        # st.Page('About_Owl.py', icon='🦉')],
            }
 
 pg = st.navigation(pages)
