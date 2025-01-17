50d9efc252e0e5039eb47c22c07f61e1d1c16533
Martin-Daniel Lacasse <mdlacasse@users.noreply.github.com>
2025-01-17 18:21:26 -0500

    rework quick start

--- ui/Quick_Start.py
+++ ui/Quick_Start.py
@@ -11,22 +11,24 @@ with col1:
     kz.orangeDivider()
     st.markdown('''
 Owl uses two files to store the specifications of a case so that its state can be recalled in the future.
-A *case* parameter file
+First, a *case* parameter file
 contains account balances, asset allocation, social security and pension, optimization
 parameter and other specifications.
 This file is in *toml* format which is editable with a simple text editor.
-A *wages and contributions* file is a
+Second, a *wages and contributions* file contains a
 time table containing anticipated wages, future contributions
 to savings accounts, and anticipated big-ticket items, which can be either expenses or income.
-This file is in Excel or LibreOffice format, and contains one tab per individual in the plan.
+This file is in Excel or LibreOffice format, and has one tab per individual in the plan.
 
 With these files, a case can be run in only a few steps. We will use the case
 of Jack and Jill provided here as an example:
 1) Download these two files from the repository:
-    - [case parameter file](https://raw.github.com/mdlacasse/Owl/main/examples/case_jack+jill.toml)
-named *case_jack+jill.toml* in editable *toml* format.
-    - [wages and contributions file](https://raw.github.com/mdlacasse/Owl/main/examples/jack+jill.xlsx)
-named *jack+jill.xlsx* in Excel format.
+    - Case parameter file named
+    [case_jack+jill.toml](https://raw.github.com/mdlacasse/Owl/main/examples/case_jack+jill.toml)
+    in editable *toml* format.
+    - Wages and contributions file named
+    [jack+jill.xlsx](https://raw.github.com/mdlacasse/Owl/main/examples/jack+jill.xlsx)
+    in Excel format.
 1) Navigate to the ***Basic Info*** page and drag and drop the case parameter file
 you downloaded called *case_jack+jill.toml*.
 1) Navigate to the ***Wages and Contributions*** page and
