e9c19e3cfb61e0f527cea9f1580f2033b4a79730
Martin-Daniel Lacasse <mdlacasse@users.noreply.github.com>
2025-01-20 17:15:15 -0500

    update version

--- ui/About_Owl.py
+++ ui/About_Owl.py
@@ -7,27 +7,23 @@ import owlbridge as owb
 st.write("## About Owl 🦉")
 kz.orangeDivider()
 
-st.write('This version of Owl was released in January 2025 (version %s).' % owb.version())
-st.write('Running on Streamlit %s.' % st.__version__)
+st.write("This version of Owl was released in January 2025 (version %s). "
+         "Running on Streamlit %s." % (owb.version(), st.__version__))
 st.snow()
 
 st.write('''
-- This code was released under GPL Licence through a publicly available
+- Owl was released under GPL Licence through a publicly available
 repository on [github](https://github.com/mdlacasse/owl).
-
-- This code does not store or forward any information. It fully respects privacy.
-All source code is provided and can be inspected in the repository.
 
 - Mathematical formulation of the linear optimization problem can be
 found [here](https://raw.github.com/mdlacasse/Owl/main/docs/owl.pdf).
 
 Copyright &copy; 2024 - Martin-D. Lacasse
 
----------------------------------------------------------------------
 #### :orange[Credits]
 - Historical rates are from [Aswath Damodaran](https://pages.stern.nyu.edu/~adamodar/).
 - Owl image is from [freepik](https://freepik.com).
-- Optimization solver is from [HiGHS](https://highs.dev).
+- Linear programming optimization solver is from [HiGHS](https://highs.dev).
 - Owl planner relies on the following [Python](https://python.org) packages:
     - [Numpy](https://numpy.org), [Matplotlib](https://matplotlib.org),
     [Pandas](https://pandas.pydata.org),
@@ -38,9 +34,11 @@ Copyright &copy; 2024 - Martin-D. Lacasse
  and [Streamlit](https://streamlit.io) for the front-end.
 - The :streamlit: [Streamlit Community Cloud](https://streamlit.io/cloud) for hosting.
 
+#### :orange[Privacy]
+- This code does not store or forward any information. It fully respects privacy.
+All source code is provided and can be inspected in the repository.
+
 #### :orange[Disclaimers]
-- I am not a financial planner.
-- You make your own decisions.
-- This program comes with no guarantee.
-- Use at your own risk.
+- I am not a financial planner. You make your own decisions.
+- This program comes with no guarantee. Use at your own risk.
 ''')
