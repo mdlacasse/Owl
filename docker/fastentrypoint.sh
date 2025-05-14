173b5df4d96a74dfcb51762a7a5a3da73871ea80
Martin-Daniel Lacasse <mdlacasse@users.noreply.github.com>
2025-05-14 13:40:12 -0400

    streamline requirements - update docker

--- docker/fastentrypoint.sh
+++ docker/fastentrypoint.sh
@@ -1,9 +1,12 @@
 #/bin/bash
 
+WORKDIR=/app/owl
+export PYTHONPATH=${WORKDIR}/src:${PYTHONPATH}
+
 echo "Owl is now running locally: Point your browser to http://localhost:8501"
 if type -P streamlit >& /dev/null; then
-    streamlit run /app/owl/ui/main.py --server.port=8501 --server.address=0.0.0.0 --browser.gatherUsageStats=false
+    streamlit run ${WORKDIR}/ui/main.py --server.port=8501 --server.address=0.0.0.0 --browser.gatherUsageStats=false
 else
-    python3 -m streamlit run /app/owl/ui/main.py --server.port=8501 --server.address=0.0.0.0 --browser.gatherUsageStats=false
+    python3 -m streamlit run ${WORKDIR}/ui/main.py --server.port=8501 --server.address=0.0.0.0 --browser.gatherUsageStats=false
 fi
 
