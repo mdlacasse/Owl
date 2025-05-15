6cfb4ff3dcb2e09f7d720dfdf30de5d01a056ec4
Martin-Daniel Lacasse <mdlacasse@users.noreply.github.com>
2025-05-15 19:14:39 -0400

    include config for dark mode

--- owlplanner.sh
+++ owlplanner.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 echo Hit Ctrl-C to terminate the server.
-cd ui
-export PYTHONPATH="../src:${PYTHONPATH}"
-if type -P streamlit; then
-    streamlit run main.py --browser.gatherUsageStats=false --browser.serverAddress=localhost
+export PYTHONPATH="./src:${PYTHONPATH}"
+if type -P streamlit >& /dev/null; then
+    streamlit run ./ui/main.py --browser.gatherUsageStats=false --browser.serverAddress=localhost
 else
-    python3 -m streamlit run main.py --browser.gatherUsageStats=false --browser.serverAddress=localhost
+    python3 -m streamlit run ./ui/main.py --browser.gatherUsageStats=false --browser.serverAddress=localhost
 fi
