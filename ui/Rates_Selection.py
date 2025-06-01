311e4727b9af409a484eb5d2229a76c15cac7de8
Martin-Daniel Lacasse <mdlacasse@users.noreply.github.com>
2025-06-01 16:07:15 -0400

    add missing help argument

--- ui/Rates_Selection.py
+++ ui/Rates_Selection.py
@@ -228,7 +228,7 @@ Unless historical, S&P 500 can represent any mix of equities
     with col1:
         kz.initKey("gainTx", 15)
         helpmsg = "Choose 0, 15, or 20% depending on anticipated total income."
-        ret = kz.getNum("Long-term capital gains tax rate (%)", "gainTx", max_value=20.0, step=5.0)
+        ret = kz.getNum("Long-term capital gains tax rate (%)", "gainTx", max_value=20.0, help=helpmsg, step=5.0)
 
     with col2:
         kz.initKey("heirsTx", 30)
