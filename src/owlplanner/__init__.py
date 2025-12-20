2a7eac91a3d87d71722a4cddaadddbb3f2ced124
John Leonard <john@lowkeylabs.com>
2025-12-20 12:13:09 -0500

    ran pytest; cleaned exports

--- src/owlplanner/__init__.py
+++ src/owlplanner/__init__.py
@@ -1,8 +1,21 @@
-from owlplanner.core.plan import Plan                                              # noqa: F401
-from owlplanner.core.plan import clone                                             # noqa: F401
-from owlplanner.core.config import readConfig                                      # noqa: F401
-from owlplanner.core.rates import getRatesDistributions                            # noqa: F401
-from owlplanner.version import __version__                                    # noqa: F401
 
-# Make the package importable as 'owlplanner'
-__all__ = ['Plan', 'clone', 'readConfig', 'getRatesDistributions', '__version__']
+# Public API re-exports
+
+from owlplanner.core.plan import Plan, clone            # noqa: F401
+from owlplanner.core.config import readConfig           # noqa: F401
+from owlplanner.core.rates import getRatesDistributions # noqa: F401
+from owlplanner.core import mylogging                   # noqa: F401
+from owlplanner.core import utils                       # noqa: F401
+from owlplanner.core import socialsecurity              # noqa: F401
+from owlplanner.version import __version__              # noqa: F401
+
+__all__ = [
+    "Plan",
+    "clone",
+    "readConfig",
+    "getRatesDistributions",
+    "mylogging",
+    "utils",
+    "socialsecurity",
+    "__version__",
+]
