04260b74dd6c706f62e034454ab490383282c051
Martin-D. Lacasse <martin.d.lacasse@gmail.com>
2026-02-21 21:58:00 -0500

    Single-source credits via CREDITS.md [no ci]
    Create CREDITS.md as canonical credits content; update README.md to link
    to it; update ui/About_Owl.py to read CREDITS.md at runtime. Also add
    click and pydantic to the package list.

--- README.md
+++ README.md
@@ -40,36 +40,8 @@ Follow these [instructions](INSTALL.md) to install from the source code and self
 
 ---------------------------------------------------------------------
 
-## Credits
-- Contributors:
- Robert E. Anderson (NH-RedAnt) for bug fixes and suggestions,
- Clark Jefcoat (hubcity) for fruitful interactions,
- kg333 for fixing an error in Docker's instructions,
- John Leonard (jleonard99) for great suggestions, website, logger, stochastic rate generation, and more to come,
- Benjamin Quinn (blquinn) for improvements and bug fixes,
- Dale Seng (sengsational) for great insights, testing, and suggestions,
- Josh Williams (noimjosh) for Docker image code,
- Gene Wood (gene1wood) for improvements and bug fixes.
-- Greg Grothaus for developing [ssa.tools](https://ssa.tools) and providing an integration with Owl.
-- Owl image is from [freepik](https://freepik.com).
-- Historical rates are from [Aswath Damodaran](https://pages.stern.nyu.edu/~adamodar/).
-- Linear programming optimization solvers are from
-[HiGHS](https://highs.dev) and [PuLP](https://coin-or.github.io/pulp/).
-It can also run on [MOSEK](https://mosek.com) if available on your computer.
-- Owl planner relies on the following [Python](https://python.org) packages:
-    - [highspy](https://highs.dev),
-    [loguru](https://github.com/Delgan/loguru),
-    [Matplotlib](https://matplotlib.org),
-    [Numpy](https://numpy.org),
-    [odfpy](https://https://pypi.org/project/odfpy),
-    [openpyxl](https://openpyxl.readthedocs.io),
-    [Pandas](https://pandas.pydata.org),
-    [Plotly](https://plotly.com),
-    [PuLP](https://coin-or.github.io/pulp),
-    [Scipy](https://scipy.org),
-    [Seaborn](https://seaborn.pydata.org),
-    [toml](https://toml.io),
- and [Streamlit](https://streamlit.io) for the front-end.
+## Credits and Acknowledgements
+See [CREDITS.md](CREDITS.md).
 
 ## Bugs and Feature Requests
 Please submit bugs and feature requests through
--- ui/About_Owl.py
+++ ui/About_Owl.py
@@ -20,6 +20,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+from pathlib import Path
+
 import streamlit as st
 
 import sskeys as kz
@@ -32,6 +34,13 @@ kz.divider("orange")
 st.markdown(f"This is Owl version {owb.version()} running on Streamlit {st.__version__}.")
 # st.balloons()
 
+credits_md = (Path(__file__).parent.parent / "CREDITS.md").read_text()
+credits_md = credits_md.replace(
+    "## Credits and Acknowledgements",
+    "#### :orange[Credits and Acknowledgements]",
+    1,
+)
+
 st.markdown(
     """
 - Owl is an open-source retirement financial planner capable of optimization through
@@ -42,42 +51,13 @@ found in this [document](https://github.com/mdlacasse/Owl/blob/main/papers/owl.p
 
 Copyright &copy; 2025-2026 - The Owlplanner [Authors](https://github.com/mdlacasse/Owl/blob/main/AUTHORS)
 
-#### :orange[Credits and Acknowledgements]
-- Original author:
-Martin-D. Lacasse (mdlacasse)
+"""
+)
 
-- Contributors (alphabetical order):
- Robert E. Anderson (NH-RedAnt) for bug fixes and suggestions,
- Clark Jefcoat (hubcity) for fruitful interactions,
- kg333 for fixing an error in Docker's instructions,
- John Leonard (jleonard99) for great suggestions, website, improved logger,
- stochastic rate generation, and more to come,
- Benjamin Quinn (blquinn) for improvements and bug fixes,
- Dale Seng (sengsational) for great insights, testing, bug fixes, and suggestions,
- Josh Williams (noimjosh) for Docker image code,
- Gene Wood (gene1wood) for improvements and bug fixes.
+st.markdown(credits_md)
 
-- Greg Grothaus for developing [ssa.tools](https://ssa.tools) and providing an integration with Owl.
-- [MOSEK](https://mosek.com) for providing a free license of their commercial software for Owl's deployment on
-the Community Cloud server.
-- [Aswath Damodaran](https://pages.stern.nyu.edu/~adamodar/) for sharing his analyses of historical rates.
-- [HiGHS](https://highs.dev) and [PuLP](https://coin-or.github.io/pulp/) for providing open-source
-linear programming optimization solvers.
-- Owl planner relies on the following [Python](https://python.org) packages:
-    [highspy](https://highs.dev),
-    [Matplotlib](https://matplotlib.org),
-    [Numpy](https://numpy.org),
-    [odfpy](https://pypi.org/project/odfpy),
-    [openpyxl](https://openpyxl.readthedocs.io),
-    [Pandas](https://pandas.pydata.org),
-    [Plotly](https://plotly.com),
-    [PuLP](https://coin-or.github.io/pulp),
-    [Scipy](https://scipy.org),
-    [Seaborn](https://seaborn.pydata.org),
-    [toml](https://toml.io),
-and [Streamlit](https://streamlit.io) for the front-end.
-- Owl image is from [freepik](https://freepik.com).
-
+st.markdown(
+    """
 #### :orange[Bugs and Feature Requests]
 Please submit bugs and feature requests through
 [GitHub](https://github.com/mdlacasse/Owl/issues) if you have a GitHub account
