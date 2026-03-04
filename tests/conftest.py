e91c14a8453142664a64b9068a23a2d88f2bcf32
Martin-D. Lacasse <martin.d.lacasse@gmail.com>
2026-03-04 01:02:33 -0500

    Add missing GPL copyright headers to 6 test files.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,21 @@ test modules so that tests are immune to calendar year rollover.  When the
 project is deliberately advanced to a new tax year, update _FROZEN_DATE below,
 increment birth years in examples/*.toml, and refresh the expected values in
 test_toml_cases.py.
+
+Copyright (C) 2025-2026 The Owlplanner Authors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import sys
