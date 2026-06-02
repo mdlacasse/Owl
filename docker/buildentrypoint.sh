8539cd4fb594ede4fa6d3af23b299008ef0f73e8
Martin-D. Lacasse <martin.d.lacasse@gmail.com>
2026-06-02 09:22:16 -0400

    [feat] migrate docker images to uv for faster startup [no ci]

--- docker/buildentrypoint.sh
+++ docker/buildentrypoint.sh
@@ -6,11 +6,10 @@ set -e
 rm -rf /app/owl
 cd /app && git clone --depth 1 https://github.com/mdlacasse/Owl.git owl
 
-export OWLDIR=/app/owl
+export OWL_DIR=/app/owl
+cd $OWL_DIR
 
-source /app/venv/bin/activate
-python -m pip install --no-cache-dir --upgrade pip
-cd $OWLDIR
-grep -vi mosek requirements.txt | python -m pip install --no-cache-dir -r /dev/stdin
+uv venv 
+grep -vi mosek requirements.txt | uv pip install --no-cache-dir -r /dev/stdin
 
 exec /usr/bin/runentrypoint.sh
--- docker/runentrypoint.sh
+++ docker/runentrypoint.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
 
-if ! [[ -v OWLDIR ]]; then
-    OWLDIR=/app/owl
+if ! [[ -v OWL_DIR ]]; then
+    OWL_DIR=/app/owl
 fi
 
-if [[ -f /app/venv/bin/activate ]]; then
-    source /app/venv/bin/activate
-fi
-
-export PYTHONPATH=${OWLDIR}/src:${PYTHONPATH}
+export PYTHONPATH=${OWL_DIR}/src:${PYTHONPATH}
 
 echo ""
 echo "Owl is now running locally: Point your browser to http://localhost:8501"
 echo "Other network addresses are turned off for security reasons."
 
-if type -P streamlit >& /dev/null; then
-    streamlit run ${OWLDIR}/ui/main.py --server.port=8501 --server.address=0.0.0.0 --browser.gatherUsageStats=false
-else
-    python3 -m streamlit run ${OWLDIR}/ui/main.py --server.port=8501 --server.address=0.0.0.0 --browser.gatherUsageStats=false
-fi
+cd ${OWL_DIR}
+uv run streamlit run ${OWL_DIR}/ui/main.py --server.port=8501 --server.address=0.0.0.0 --browser.serverAddress=localhost --browser.gatherUsageStats=false
 
